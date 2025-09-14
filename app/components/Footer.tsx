e23e4eaedbfa47914d659a5cf4d070827d408626
sabler <gizmo.00bases@icloud.com>
2025-09-13 20:07:07 -0700

    Auto update the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,11 @@
 const Footer: React.FC = () => {
+
+  const currentYear = new Date().getFullYear();
+
     return (
       <>
         <footer className='flex w-full place-content-center text-center text-xs items-center'>
-          &copy; 2023-2024 rob sable <br />
+          &copy; 2023-{currentYear} rob sable <br />
           Made with ❤️ on planet earth
         </footer>
       </>
